fa8faf7b6bd37b46630227eb4bf01d8a76865f48
Jingheng <jh730@duke.edu>
2023-07-13 22:14:41 +0800

    CS193P get started!

---
+++ CS193P-Demo/CS193P-Demo/CS193P_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193P_DemoApp.swift
+//  CS193P-Demo
+//
+//  Created by 郇敬恒 on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct CS193P_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
