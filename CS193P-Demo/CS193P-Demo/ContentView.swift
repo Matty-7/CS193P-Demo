88d56872590affca92eceb85b553d8702aa41b3f
Jingheng <jh730@duke.edu>
2023-07-14 16:43:35 +0800

    Finish add card and remove card function

--- CS193P-Demo/CS193P-Demo/ContentView.swift
+++ CS193P-Demo/CS193P-Demo/ContentView.swift
@@ -8,28 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚔", "🚍", "🚗", "🚕", "🚙", "🚌", "🚎", "🏎️", "🚓", "🚑", "🚒", "🚐", "🛻", "🚚", "🚛", "🚜", "🛵", "🏍️", "🛺", "🚘", "🚖", "✈️", "🚁", "🚀"]
+    @State var emojiCount: Int = 6
     var body: some View {
-        HStack {
-            CardView(content: "🚀")
-            CardView(content: "🚁")
-            CardView(content: "🛰️")
-            CardView(content: "✈️")
+        VStack {
+            HStack {
+                ForEach(emojis[0..<emojiCount], id: \.self, content: { emoji in
+                    CardView(content: emoji)
+                })
+            }
+            HStack{
+                remove
+                Spacer()
+                add
+            }
+            .padding(.horizontal)
         }
         .padding(.horizontal)
         .foregroundColor(.red)
     }
+    var remove: some View {
+        Button(action: {
+            emojiCount -= 1
+        }, label:{
+            VStack{
+                Text("Remove")
+                Text("Card")
+            }})
+    }
+    var add: some View {
+        Button(action: {
+            emojiCount += 1
+        }, label:{
+            VStack{
+                Text("Add")
+                Text("Card")
+            }})
+    }
 }
- 
+    
+
 struct CardView: View {
     var content: String
     @State var isFaceUp: Bool = true
     var body: some View {
+        
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 20)
             if isFaceUp {
                 shape.fill().foregroundColor(.white)
                 shape.stroke(lineWidth: 3)
-                Text("✈️").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 shape.fill()
             }
@@ -39,12 +68,12 @@ struct CardView: View {
         }
     }
     
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-                .preferredColorScheme(.light)
-            ContentView()
-                .preferredColorScheme(.dark)
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
         }
     }
 }
